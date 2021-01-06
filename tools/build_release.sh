7bbfec8a3bfd1b20c359a5c4a972d097c7417044
Leon <me@ljs.dev>
2021-01-06 14:22:22 +1000

    wip fixing some copied references

--- tools/build_release.sh
+++ tools/build_release.sh
@@ -17,8 +17,8 @@ TMP_DIR=$HOME/plugintmp
 rm -Rf $TMP_DIR
 mkdir -p $TMP_DIR
 
-rm -Rf $TMP_DIR/wp2static-addon-s3
-mkdir $TMP_DIR/wp2static-addon-s3
+rm -Rf $TMP_DIR/wp2static-addon-gcs
+mkdir $TMP_DIR/wp2static-addon-gcs
 
 # clear dev dependencies
 rm -Rf $EXEC_DIR/vendor/*
@@ -26,10 +26,10 @@ rm -Rf $EXEC_DIR/vendor/*
 composer install --quiet --no-dev --optimize-autoloader
 
 # cp all required sources to build dir
-cp -r $EXEC_DIR/*.php $TMP_DIR/wp2static-addon-s3/
-cp -r $EXEC_DIR/src $TMP_DIR/wp2static-addon-s3/
-cp -r $EXEC_DIR/vendor $TMP_DIR/wp2static-addon-s3/
-cp -r $EXEC_DIR/views $TMP_DIR/wp2static-addon-s3/
+cp -r $EXEC_DIR/*.php $TMP_DIR/wp2static-addon-gcs/
+cp -r $EXEC_DIR/src $TMP_DIR/wp2static-addon-gcs/
+cp -r $EXEC_DIR/vendor $TMP_DIR/wp2static-addon-gcs/
+cp -r $EXEC_DIR/views $TMP_DIR/wp2static-addon-gcs/
 
 cd $TMP_DIR
 
@@ -37,7 +37,7 @@ cd $TMP_DIR
 find . -type d -exec chmod 755 {} \;
 find . -type f -exec chmod 644 {} \;
 
-zip --quiet -r -9 ./$1.zip ./wp2static-addon-s3
+zip --quiet -r -9 ./$1.zip ./wp2static-addon-gcs
 
 cd -
 
